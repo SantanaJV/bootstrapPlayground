da48088b648f0476a564233adf1589fd5492c620
João Santana <joaosantana2001@icloud.com>
2019-06-29 13:02:32 -0300

    Improved Calculator efficiency, working with strings instead pure math.

--- playground/src/app/calculator/calculator.component.ts
+++ playground/src/app/calculator/calculator.component.ts
@@ -6,95 +6,30 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./calculator.component.css", "../app.component.css"]
 })
 export class CalculatorComponent implements OnInit {
+  input = "";
   result = 0;
-  input = 0;
-  previous = 0; // 1 = ADD, 2 = SUBTRACT, 3 = MULTIPLY, 4 = DIVIDE
-  equalsWasPressed = false;
 
   constructor() {}
 
   ngOnInit() {}
 
-  clearInput() {
-    this.input = 0;
-  }
-
-  clear() {
-    this.input = 0;
-    this.result = 0;
-    this.previous = 0;
-  }
-
-  erase() {
-    this.input = Math.floor(this.input / 10);
-  }
-
   digit(digit) {
-    this.input *= 10;
     this.input += digit;
   }
 
-  operate() {
-    switch (this.previous) {
-      case 0:
-        this.result += this.input;
-        break;
-      case 1:
-        this.result += this.input;
-        break;
-      case 2:
-        this.result -= this.input;
-        break;
-      case 3:
-        this.result *= this.input;
-        break;
-      case 4:
-        this.result = this.input != 0 ? this.result / this.input : 0;
-    }
-  }
-
-  add() {
-    if (this.equalsWasPressed) {
-      this.equalsWasPressed = false;
-    } else {
-      this.operate();
-    }
-    this.previous = 1;
-    this.input = 0;
-  }
-
-  subtract() {
-    if (this.equalsWasPressed) {
-      this.equalsWasPressed = false;
-    } else {
-      this.operate();
-    }
-    this.previous = 2;
-    this.input = 0;
-  }
-
-  multiply() {
-    if (this.equalsWasPressed) {
-      this.equalsWasPressed = false;
-    } else {
-      this.operate();
-    }
-    this.previous = 3;
-    this.input = 0;
-  }
-
-  divide() {
-    if (this.equalsWasPressed) {
-      this.equalsWasPressed = false;
-    } else {
-      this.operate();
-    }
-    this.previous = 4;
-    this.input = 0;
-  }
-
   equals() {
-    this.operate();
-    this.equalsWasPressed = true;
+    let exp = Function("return " + this.input)();
+    this.result = exp ? exp : "Error";
+  }
+
+  clear() {
+    this.input = "";
+    this.result = 0;
+  }
+
+  erase() {
+    let exp = this.input;
+    exp = exp.substring(0, exp.length - 1);
+    this.input = exp;
   }
 }
