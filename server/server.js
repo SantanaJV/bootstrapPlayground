268e589b3c89b8fdee23cc5bf2a07bebebd5f7b7
João Santana <joaosantana2001@icloud.com>
2019-06-30 14:00:13 -0300

    Added the API route.

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const api = require("./routes/api");
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use("/api", api);
 
 app.get("/", (req, res) => {
   res.send("Welcome to the server.");
