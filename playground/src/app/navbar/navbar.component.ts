f90e927d8339ed5d65a57052f1ef1562af6a876c
João Santana <joaosantana2001@icloud.com>
2019-07-01 14:30:33 -0300

    Created a token interceptor to populate the Authorization Header on http requests.

--- playground/src/app/navbar/navbar.component.ts
+++ playground/src/app/navbar/navbar.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { AuthService } from "../auth.service";
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  selector: "app-navbar",
+  templateUrl: "./navbar.component.html",
+  styleUrls: ["./navbar.component.css"]
 })
 export class NavbarComponent implements OnInit {
+  constructor(private auth: AuthService) {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
