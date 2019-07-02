bcfa972f10c7d33df827fe723cddc9335268ca53
João Santana <joaosantana2001@icloud.com>
2019-07-02 10:37:44 -0300

    Added game data to User model.

--- server/models/user.js
+++ server/models/user.js
@@ -11,6 +11,15 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
     max: 255
+  },
+  gameData: {
+    number: Number,
+    producer: [
+      {
+        amount: Number,
+        level: Number
+      }
+    ]
   }
 });
 
